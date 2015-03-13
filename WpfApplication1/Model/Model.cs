0a8477f598a5d5c1d0328600a041df44f83db512
dan.krasniak <dan.krasniak@gmail.com>
2015-03-13 16:53:45 +0100

    Vanilla 1.0

---
+++ WpfApplication1/Model/Model.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfApplication1.Model
+{
+    public class Model
+    {
+         
+    }
+}
---
+++ WpfApplication1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfApplication1.ViewModel
+{
+    public class MainWindowViewModel
+    {
+         
+    }
+}
